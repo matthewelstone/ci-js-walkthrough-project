dea6d5cdaab088d3dc08d60462df5e7e245eae53
Matt Elstone <79518948+matthewelstone@users.noreply.github.com>
2021-07-01 11:07:08 +0100

    added eventListener functions

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+document.addEventListener("DOMContentLoaded", function(){
+  let buttons = document.getElementsByTagName("button");
+
+
+  for (let button of buttons){
+    button.addEventListener("click", function(){
+      if (this.getAttribute("data-type")==="submit"){
+        alert("You Clicked Submit!");
+      } else{
+        let gameType = this.getAttribute("data-type");
+        alert(`You Clicked ${gameType}`);
+      }
+    }) 
+  }
+})
+
 function runGame(){
 
+
+
+  
 }
 
 function checkAnswer(){
